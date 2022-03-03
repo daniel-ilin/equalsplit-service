3fc3ab34b415d7935bc5423b8284b03da228c4a9
Daniel Ilin <danielilin@Daniels-MacBook-Pro.local>
2022-03-02 19:00:21 -0500

    Fixed ssl for production

--- db/index.js
+++ db/index.js
@@ -2,10 +2,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false,    
-  // }
-  ssl: process.env.DATABASE_URL ? true : false
+  ssl: {
+    rejectUnauthorized: false
+  }  
 });
 
 

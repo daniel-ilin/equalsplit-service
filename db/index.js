10904e0baa2d36d2084f1dc9b44947b7b1e58d2d
Daniel Ilin <danielilin@Daniels-MacBook-Pro-2.local>
2022-04-24 16:28:51 -0400

    configured ssl

--- db/index.js
+++ db/index.js
@@ -3,9 +3,10 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.ISLOCAL == false ? {
-    rejectUnauthorized: false
-  } : false
+  // ssl: {
+  //   rejectUnauthorized: false
+  // } 
+  ssl : false
 });
 
 

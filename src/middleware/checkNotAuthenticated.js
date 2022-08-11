1e1c2573cd08198ef53890133e5f61924e4dff04
Daniel Ilin <danielilin@Daniels-MacBook-Pro-2.local>
2022-08-10 21:44:12 -0400

    troubleshoot

--- src/middleware/checkNotAuthenticated.js
+++ src/middleware/checkNotAuthenticated.js
@@ -1,7 +1,10 @@
 function checkNotAuthenticated(req, res, next) {
+  console.log("Checking not authenticated!");
   if (req.isAuthenticated()) {    
+    console.log("Bro you're authenticated!");
     return res.redirect("/login/success");
   } else {    
+    console.log("Not authenticated");
     return next();
   }
 }
